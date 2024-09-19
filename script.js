7df2dd49b8ad4dca2697941520837d958705fe52
bogdanovM <maxkrasnodar3@gmail.com>
2024-09-19 14:51:27 +0300

    updated

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ async function getGeo(callback) {
 async function getWeather(coordinates) {
     const xhr = new XMLHttpRequest();
     const appid = '';
+    // xhr.open('GET', `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${appid}&units=metric&lang=ru`, true);
     xhr.open('GET',`https://testovoe-sable.vercel.app/api/weather?lat=${coordinates.lat}&lon=${coordinates.lon}`);
     xhr.send();
     xhr.onload = function () {
